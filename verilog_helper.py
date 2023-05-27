ec28a3ecd29d9bab0d0e7cd6f88c6074ddbbfaac
이창민 <lcentimeter@hanyang.ac.kr>
2023-05-27 18:51:42 +0900

    Update verilog_helper.py

--- verilog_helper.py
+++ verilog_helper.py
@@ -1,11 +1,11 @@
-file_path = "instruction_project2_hex.txt"
+file_path = "instruction_hex.txt"
 with open(file_path) as f:
     codes = f.readlines()
     codes = list(map(lambda s: s.strip(), codes))
     
     
     
-with open('verilog_project2.txt','w',encoding='UTF-8') as f:
+with open('verilog_project.txt','w',encoding='UTF-8') as f:
     i=0
     for code in codes:
         f.write("Inst_mem["+str(i)+"] = 32'h"+code.replace('0x','')+";"+'\n')
